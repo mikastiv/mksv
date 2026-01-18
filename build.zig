25a68b6ed8f700851fb97a5a8371bd7e3d84d4a8
mikastiv <mikastiv@outlook.com>
2026-01-17 21:50:01 -0400

    initial

---
+++ build.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "qoi_image",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+
+    b.installArtifact(exe);
+}
---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "zig flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    zig.url = "github:mitchellh/zig-overlay";
+    zls.url = "github:zigtools/zls";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, zig, zls, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            zig.packages.${system}."0.15.1"
+            zls.packages.${system}.zls
+            # glfw
+            # vulkan-loader
+            # vulkan-headers
+            # vulkan-tools
+            # vulkan-validation-layers
+            # vulkan-tools-lunarg
+            # shaderc
+            lldb
+          ];
+        };
+      });
+}
+
