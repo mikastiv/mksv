2c28f462ac384de34f25a9132f70b59bf492b541
mikastiv <mikastiv@outlook.com>
2026-03-04 12:00:46 -0400

    change name

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
 
     const exe = b.addExecutable(.{
-        .name = "qoi_image",
+        .name = "mksv",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = target,
