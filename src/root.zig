306485e40f246ca172602e428fc1a702519d2025
mikastiv <mikastiv@outlook.com>
2026-05-26 16:02:49 -0300

    bounded_array

--- src/root.zig
+++ src/root.zig
@@ -1,7 +1,12 @@
 pub const image = @import("image.zig");
 pub const hash = @import("hash.zig");
 
+const bounded_array = @import("bounded_array.zig");
+pub const BoundedArray = bounded_array.BoundedArray;
+pub const BoundedArrayAligned = bounded_array.BoundedArrayAligned;
+
 test {
     _ = image;
     _ = hash;
+    _ = bounded_array;
 }
